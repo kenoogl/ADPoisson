082fb2b82d14a064b6b292fa3e0cce99c096bd26
Kenji Ono <kenoogl@gmail.com>
2026-02-04 00:52:25 +0900

    feat: log run config

--- scripts/main.jl
+++ scripts/main.jl
@@ -3,16 +3,7 @@
 using ADPoisson
 
 function parse_args(args)
-    opts = Dict{String,Any}()
-    opts["nx"] = 16
-    opts["ny"] = 16
-    opts["nz"] = 16
-    opts["M"] = 10
-    opts["dt"] = 1e-4
-    opts["tend"] = 1.0
-    opts["epsilon"] = 1e-10
-    opts["alpha"] = 1.0
-    opts["bc_order"] = "spec"
+    opts = default_cli_options()
 
     i = 1
     while i <= length(args)
@@ -23,8 +14,14 @@ function parse_args(args)
             end
             if key == "bc-order" || key == "bc_order"
                 opts["bc_order"] = args[i + 1]
+            elseif key == "max-steps" || key == "max_steps"
+                opts["max_steps"] = parse(Int, args[i + 1])
             else
-                opts[key] = parse(Float64, args[i + 1])
+                if key == "nx" || key == "ny" || key == "nz" || key == "M"
+                    opts[key] = parse(Int, args[i + 1])
+                else
+                    opts[key] = parse(Float64, args[i + 1])
+                end
             end
             i += 2
         else
@@ -37,9 +34,10 @@ end
 function main()
     opts = parse_args(ARGS)
     config = SolverConfig(Int(opts["nx"]), Int(opts["ny"]), Int(opts["nz"]),
-                          Int(opts["M"]), opts["dt"], opts["tend"], opts["epsilon"])
+                          Int(opts["M"]), opts["dt"], Int(opts["max_steps"]), opts["epsilon"])
     prob, _ = make_problem(config; alpha=opts["alpha"])
     bc_order = Symbol(opts["bc_order"])
+    @info "run config" nx=config.nx ny=config.ny nz=config.nz M=config.M dt=config.dt max_steps=config.max_steps epsilon=config.epsilon alpha=prob.alpha bc_order=bc_order
     sol = solve(config, prob; bc_order=bc_order)
     plot_slice(sol, prob, config)
     @info "done" t=sol.t iter=sol.iter
